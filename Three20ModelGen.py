c5d182473e3880c50891a46ad9019c97f00f2338
Sumeru Chatterjee <samlearnstorock@gmail.com>
2011-06-14 22:47:00 -0400

    Now parsing subobjects

--- Three20ModelGen.py
+++ Three20ModelGen.py
@@ -12,7 +12,7 @@ hackerMatch = re.search(r'(hacker)\s(.+)',model,re.IGNORECASE)
 
 if hackerMatch:
     hacker = hackerMatch.group(2)
-    print 'Hello '+hacker+' T20Gen will now generate code for you..'
+    print 'Hello '+hacker+' T20Gen will now generate code for you...\n'
 else:
     print 'Hacker name not found,using \'samyzee\' as Hacker Name'
     hacker = 'samyzee'
@@ -20,18 +20,45 @@ else:
 
 ## Search for objects and generate object files
 
-objects = re.findall(r'Object\s.*',model,re.IGNORECASE)
+objects = re.findall(r"Object\s([\w ]*)\s+(.*?)(?:(?:\nEnd\s*\n)|\Z)",model,re.DOTALL)
 
 if not objects:
         print "No objects were found in your model File"
 else:
+        print "Parsing Objects..."
         for object in objects:
+                interfaces = re.split(r'\W+',object[0])
                 
-                
+                objectclass = interfaces[0]
 
+                if(len(interfaces)<2):
+                   superclass = 'NSObject'
+                else:
+                   superclass = interfaces[1]
+
+                print objectclass+':'+superclass
+                
+                subobjects = re.split(r'[\n\t]+',object[1])
+
+                for subobject in subobjects:
+                        
+                        subobjectsplit = re.split(r'\W+',subobject)        
+
+                        subobjectclass = subobjectsplit[0]
+                        subobjectname = ''
+                                
+                        if (len(subobjectsplit)<2):
+                                print "Error:No subobjectname found with type "+subobjectsplit[0]+"in object "+objectclass
+                        else:
+                                subobjectname = subobjectsplit[1]
+
+                        print '\t'+subobjectclass+' '+subobjectname        
+
+                
+                
+        
+                
+        
 ## Search for models and generate model files    
 
-
-
-object
     
