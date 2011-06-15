b6cef10379b75581c587fa598bea8c907079e184
Sumeru Chatterjee <sumeru@ufl.edu>
2011-06-15 03:43:21 -0400

    Parsed Objects from ttmodel file into Python objects

--- Three20ModelGen.py
+++ Three20ModelGen.py
@@ -1,64 +1,107 @@
-import struct, string ,sys, re
+import struct, string ,sys, re, pdb	
 
-if len(sys.argv)==1:
-	print "Usage T20ModelObject.py <input-source-file-name>"
+class ModelObject(object):
 
-filename = sys.argv[1];
+	def __init__ (self,objectName,objectType):
+		self.name = objectName
+		self.type = objectType
+		self.subObjects = []		
 
-model = open(filename, 'r').read()
+	def addObject(self,subObject):
+		self.subObjects.append(subObject)
+		
+	def description (self):
+		description = "<ModelObject>" + " " + self.name +" Type:"+ self.type	
+		
+		for subObject in self.subObjects:
+			description += "\n\t" + subObject.description()
+			
+		return description	
+		
+class BaseObject(object):
 
-## Search for hacker name
-hackerMatch = re.search(r'(hacker)\s(.+)',model,re.IGNORECASE)
+	def __init__ (self,baseObjectName,baseObjectType):
+		self.name = baseObjectName
+		self.type = baseObjectType
+		
+	def description (self):
+		
+		return "<BaseObject>" + " " + self.name +" Type:"+ self.type
 
-if hackerMatch:
-    hacker = hackerMatch.group(2)
-    print 'Hello '+hacker+' T20Gen will now generate code for you...\n'
-else:
-    print 'Hacker name not found,using \'samyzee\' as Hacker Name'
-    hacker = 'samyzee'
+def parse_hacker_name (data):
 
+	## parse hacker name from data 
+	hackerMatch = re.search(r'(hacker)\s(.+)',data,re.IGNORECASE)
+	
+	if hackerMatch:
+		hacker = hackerMatch.group(2)
+	else:
+		print 'Hacker name not found,using \'samyzee\' as Hacker Name'
+		hacker = 'samyzee'
+	
+	return hacker		
 
-## Search for objects and generate object files
+		
+def parse_model_objects (data):
 
-objects = re.findall(r"Object\s([\w ]*)\s+(.*?)(?:(?:\nEnd\s*\n)|\Z)",model,re.DOTALL)
+	## parse model objects from data
+	allObjects = []
+	
+	objects = re.findall(r"Object\s([\w ]*)\s+(.*?)(?:(?:\nEnd\s*\n)|\Z)",data,re.DOTALL)
 
-if not objects:
-        print "No objects were found in your model File"
-else:
-        print "Parsing Objects..."
-        for object in objects:
-                interfaces = re.split(r'\W+',object[0])
-                
-                objectclass = interfaces[0]
+	if not objects:
+		print "No objects were found in your model File"
+	else:
+		print "Parsing Objects..."
+		
+		for object in objects:
+			interfaces = re.split(r'\W+',object[0])
+			
+			objectclass = interfaces[0]
+			
+			if(len(interfaces)<2):
+				superclass = "Object"
+			else:
+				superclass = interfaces[1]	
+		
+			modelObject = ModelObject(objectclass,superclass)
+			
+			subobjects = re.split(r'[\n\t]+',object[1])
+			
+			for subobject in subobjects:
+				
+				subobjectsplit = re.split(r'\W+',subobject)
+					
+				modelObject.addObject(BaseObject(subobjectsplit[1],subobjectsplit[0]))
+			
+			allObjects.append(modelObject)
+		
+	return allObjects	
+							
+				
 
-                if(len(interfaces)<2):
-                   superclass = 'NSObject'
-                else:
-                   superclass = interfaces[1]
+# Gather our code in a main() function
+def main():
+	if len(sys.argv)==1:
+		print "Usage T20ModelObject.py <input-source-file-name>"
 
-                print objectclass+':'+superclass
-                
-                subobjects = re.split(r'[\n\t]+',object[1])
+	filename = sys.argv[1];
 
-                for subobject in subobjects:
-                        
-                        subobjectsplit = re.split(r'\W+',subobject)        
+	model = open(filename, 'r').read()
 
-                        subobjectclass = subobjectsplit[0]
-                        subobjectname = ''
-                                
-                        if (len(subobjectsplit)<2):
-                                print "Error:No subobjectname found with type "+subobjectsplit[0]+"in object "+objectclass
-                        else:
-                                subobjectname = subobjectsplit[1]
+	hacker = parse_hacker_name(model)
 
-                        print '\t'+subobjectclass+' '+subobjectname        
+	print 'Hello '+hacker+' T20Gen will now generate code for you...\n'
 
-                
-                
-        
-                
-        
-## Search for models and generate model files    
+	objects = parse_model_objects(model)
 
-    
+	for object in objects:
+		print object.description()
+		
+		
+
+## Standard boilerplate to call the main() function to begin
+## the program. 
+if __name__ == '__main__':
+  main()
+      
